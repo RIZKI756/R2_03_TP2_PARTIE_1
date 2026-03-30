3a63c88b1152a859e55575ca10d2a5397e83acf6
Bob The Builder <151051388+RIZKI756@users.noreply.github.com>
2026-03-30 14:49:48 +0200

    Modif

--- GABRIEL/HelloWorld.java
+++ GABRIEL/HelloWorld.java
@@ -3,7 +3,7 @@ package GABRIEL;
 public class HelloWorld {
    public static void main(String[] args) {
 
-        System.out.println("Hello Word !");
+        System.out.println("Hello BUT1 Info");
 
    }
 }
