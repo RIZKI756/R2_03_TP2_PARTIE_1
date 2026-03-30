93867383901545d87c7404b7b157671a635572e4
laniezgabriel <laniezgabriel59@mail.com>
2026-03-30 15:24:47 +0200

    main  gab

--- Gab/Main.java
+++ Gab/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Gab/Main.java
 package Gab;
 public class Main {
 
@@ -32,38 +31,3 @@ public class Main {
         infosBinome();
     }
 }
-=======
-public class Main {
-
-    // Calcule la moyenne de deux notes
-    public static double moyenne(double note1, double note2) {
-        return 0; 
-        
-    }
-
-    // Retourne la meilleure note
-    public static double meilleureNote(double note1, double note2) {
-        return 0; // À compléter
-    }
-
-    /*
-     * Affiche les informations du binôme
-     * Attention : chacun ajoute sa ligne
-     */
-    public static void infosBinome() {
-        // System.out.println("Nom Prénom 1");
-        // System.out.println("Nom Prénom 2");
-    }
-
-    public static void main(String[] args) {
-
-        double note1 = 12.5;
-        double note2 = 15.0;
-
-        System.out.println("La moyenne est : " + moyenne(note1, note2));
-        System.out.println("La meilleure note est : " + meilleureNote(note1, note2));
-
-        infosBinome();
-    }
-}
->>>>>>> 2a97bc7b2e557782752688e292448384abe40e14:Main.java
