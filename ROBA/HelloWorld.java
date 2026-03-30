e94703eb0aa6c433bab1a40f30c0d6f4ee9c5831
vboxuser <vboxuser@debian.myguest.virtualbox.org>
2026-03-30 14:58:17 +0200

    niquez vos meres

--- ROBA/HelloWorld.java
+++ ROBA/HelloWorld.java
@@ -2,7 +2,7 @@ package ROBA;
 public class HelloWorld {
    public static void main(String[] args) {
 
-        System.out.println("Hello Word !");
+        System.out.println("Hello BUT 1 info !");
 
    }
 }
